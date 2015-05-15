2029f6aa8c0e343b998aea20592bd764ae34b07a
shinepengwei <shinepengwei@163.com>
2015-05-15 23:38:51 +0800

    Initial Commit

---
+++ JJSTL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JJSTL
+//
+//  Created by yangpengwei on 15/5/15.
+//  Copyright (c) 2015年 yangpengwei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
